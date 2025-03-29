067dcb400e3203178c7f540182fb7c3adea0b700
Tran Thanh Vu <tranthanhvuit@gmail.com>
2025-03-29 15:04:51 +0700

    Them Cors cho phep frontend goi API

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://giaoan-gdnn-ai.vercel.app"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
